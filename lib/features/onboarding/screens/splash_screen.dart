b7782d3f70f5944dcabd1dc06a6e350309eb7149
Syahrul Mubaraq <syhrul4321@gmail.com>
2025-12-10 18:41:33 +0800

    mengubah warna background pada loading/splash screen

--- lib/features/onboarding/screens/splash_screen.dart
+++ lib/features/onboarding/screens/splash_screen.dart
@@ -1,6 +1,8 @@
+import 'package:taskmate_app/features/auth/screens/login_screen.dart';
 import 'package:flutter/material.dart';
 import '../../../core/theme.dart';
 import 'onboarding_screen.dart';
+
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -13,16 +15,37 @@ class _SplashScreenState extends State<SplashScreen> {
   void initState() {
     super.initState();
     Future.delayed(const Duration(seconds: 3), () {
-      Navigator.pushReplacement(context, MaterialPageRoute(builder: (_) => const OnboardingScreen()));
+      Navigator.pushReplacement(
+          context, MaterialPageRoute(builder: (_) => const LoginScreen()));
     });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: AppTheme.primaryColor,
-      body: Center(
-        child: Text("TaskMate", style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold, color: Colors.white)),
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        decoration: BoxDecoration(
+          gradient: RadialGradient(
+            colors: [
+              Color(0xFF000020),
+              Color(0xFF080F48),
+            ],
+            center: Alignment.center,
+            radius: 1.0,
+          ),
+        ),
+        child: Center(
+          child: Text(
+            "TaskMate",
+            style: TextStyle(
+              fontSize: 40,
+              fontWeight: FontWeight.bold,
+              color: Colors.white,
+            ),
+          ),
+        ),
       ),
     );
   }
